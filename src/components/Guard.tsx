01923809d76cda5e19f0e1c6e1c2c1fe0c44e1f2
Mohammad Ataei <mammadataei@gmail.com>
2022-08-20 11:19:44 +0430

    refactor: reorganize Guard module

--- src/components/Guard.tsx
+++ src/components/Guard.tsx
@@ -14,25 +14,33 @@ export function Guard(props: PropsWithChildren<GuardProps>) {
   const { children, fallback, policies } = props
 
   if (policies.length === 0) {
-    console.warn(
-      '[React Guardian]: No policies are provided to the Guard component. \n' +
-        'At least provide one policy, or descendants will render without protection.',
-    )
+    throwNoPoliciesAreProvidedWarning()
   }
 
   const deniedPolicy = policies.find((policy) => !policy().allowed)
 
   if (deniedPolicy !== undefined) {
     if (fallback) {
-      return createElement(
-        Fragment,
-        null,
-        typeof fallback === 'function' ? fallback(deniedPolicy()) : fallback,
-      )
+      return renderFallback(fallback, deniedPolicy)
     }
 
     return null
   }
 
   return createElement(Fragment, null, children)
+}
+
+function throwNoPoliciesAreProvidedWarning() {
+  console.warn(
+    '[React Guardian]: No policies are provided to the Guard component. \n' +
+      'At least provide one policy, or descendants will render without protection.',
+  )
+}
+
+function renderFallback(fallback: GuardFallback, deniedPolicy: Policy) {
+  return createElement(
+    Fragment,
+    null,
+    typeof fallback === 'function' ? fallback(deniedPolicy()) : fallback,
+  )
 }
